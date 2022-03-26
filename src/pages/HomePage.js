4cbd73b560fafc13b54873910dc97e1aa3296dcf
bimapriaaditya <bpabima@gmail.com>
2022-03-26 11:32:41 +0700

    finished homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,10 @@ import React from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
 import BrowseRoom from '../parts/HomePage/BrowseRoom'
+import JustArrived from '../parts/HomePage/JustArrived'
+import Clients from '../parts/Clients'
+import Aside from '../parts/Aside'
+import Footer from '../parts/Footer'
 
 export default function HomePage(props) {
   return (
@@ -10,6 +14,10 @@ export default function HomePage(props) {
       <Header />
       <Hero />
       <BrowseRoom />
+      <JustArrived />
+      <Clients />
+      <Aside />
+      <Footer />
     </>
   )
 }
