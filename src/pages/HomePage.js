79b2c7fcf2815596885204ddf5ea63d40a65fb6e
bimapriaaditya <bpabima@gmail.com>
2022-04-17 14:11:03 +0700

    custome hooks, useScrollAnchor

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 import Header from '../parts/Header'
-import Hero from '../parts/Hero'
+import Hero from '../parts/HomePage/Hero'
 import BrowseRoom from '../parts/HomePage/BrowseRoom'
 import JustArrived from '../parts/HomePage/JustArrived'
 import Clients from '../parts/Clients'
 import Aside from '../parts/Aside'
 import Footer from '../parts/Footer'
 
+import { useScrollAnchor } from '../helpers/hooks/useScrollAnchor'
+
 export default function HomePage(props) {
+  useScrollAnchor()
   return (
     <>
       <Header />
