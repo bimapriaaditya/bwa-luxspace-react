45a7fa56a538370dfd41fb333a11ada156f8d264
bimapriaaditya <bpabima@gmail.com>
2022-04-17 14:44:20 +0700

    hooks modals dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
-import './assets/css/app.css';
-import './input.css';
+import './output.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
